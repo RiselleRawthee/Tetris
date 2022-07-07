4262b56dec6507a5374cd3454591e84375798aaf
Lisa Nolte <lisan@bbd.co.za>
2022-07-07 09:17:25 +0200

    Adding tetris game logic

---
+++ tetris-game/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './reducers'
+
+import './App.css';
+
+import Controls from './components/Controls';
+import GridBoard from './components/GridBoard';
+import NextBlock from './components/NextBlock';
+import ScoreBoard from './components/ScoreBoard';
+import MessagePopup from './components/MessagePopup';
+import KeyPressElement from './components/KeyBoardControls';
+import HoldBlock from './components/HoldBlock';
+
+const store = createStore(reducers)
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Tetris Redux</h1>
+        </header>
+        <GridBoard />
+        <NextBlock />
+        <ScoreBoard />
+        <Controls />
+        <MessagePopup />
+        <KeyPressElement />
+        <HoldBlock />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ tetris-game/src/components/KeyBoardControls.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useRef, useCallback } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { pause, resume, moveDown, moveLeft, moveRight, rotate, hold } from '../actions'
+import HoldBlock from './HoldBlock';
+
+const ESCAPE_KEYS = ["ArrowLeft", "ArrowRight", "ArrowDown","ArrowUp", " ", "p"];
+
+const useEventListener = (eventName, handler, element = window) => {
+  const savedHandler = useRef();
+
+  useEffect(() => {
+    savedHandler.current = handler;
+  }, [handler]);
+
+  useEffect(() => {
+    const eventListener = (event) => savedHandler.current(event);
+    element.addEventListener(eventName, eventListener);
+    return () => {
+      element.removeEventListener(eventName, eventListener);
+    };
+  }, [eventName, element]);
+};
+
+export default function KeyPressElement() {
+  const dispatch = useDispatch()
+  const isRunning = useSelector((state) => state.game.isRunning)
+  const gameOver = useSelector((state) => state.game.gameOver)
+  const handler = ({ key }) => {
+    if (ESCAPE_KEYS.includes(String(key))) {
+        switch(String(key)){
+            case "ArrowDown":
+              if (!isRunning || gameOver) { return }
+              dispatch(moveDown())
+              break;
+              case "ArrowLeft":
+              if (!isRunning || gameOver) { return }
+              dispatch(moveLeft())
+              break;
+              case "ArrowRight":
+              if (!isRunning || gameOver) { return }
+              dispatch(moveRight())
+              break;
+              case "ArrowUp":
+              if (!isRunning || gameOver) { return }
+              dispatch(hold())
+              break;
+              case " ":
+              if (!isRunning || gameOver) { return }
+              dispatch(rotate())
+              break;
+              case "p":
+                if (isRunning) {
+                    dispatch(pause())
+                } else {
+                    dispatch(resume())
+                }
+                break;
+            default:
+              break;
+        }
+      console.log("Escape key pressed!");
+    }
+  };
+
+  useEventListener("keydown", handler);
+
+  return <span></span>;
+}
