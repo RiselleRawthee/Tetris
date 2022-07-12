8a6e8ad84e232c05a9c1e6c3ba1c17cbf2c7ba3c
bbdnet2497 <zaeema@bbd.co.za>
2022-07-12 14:02:03 +0200

    removed pm in \ applicationm_start

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -7,7 +7,7 @@ cd /home/ec2/app
 
 # start app
 npm start
-pm2 start npm --name "tetris" -- start
-pm2 startup
-pm2 save
-pm2 restart all
+# pm2 start npm --name "tetris" -- start
+# pm2 startup
+# pm2 save
+# pm2 restart all
