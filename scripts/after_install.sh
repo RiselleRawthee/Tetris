b7d2daab807678c842598a1aa27b88148157122b
bbdnet2497 <zaeema@bbd.co.za>
2022-07-12 10:25:52 +0200

    updated scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -9,5 +9,3 @@ npm install
 npm install pm2 -g
 
 # npm install --save react react-dom react-scripts react-particles-js
-
-# npm install pm2 -g
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-cd /home/ec2-user/app
+cd /home/ec2/app
 
 # start app
-npm start 
 pm2 start npm --name "tetris" -- start
 pm2 startup
 pm2 save
 pm2 restart all
-
