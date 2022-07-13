88c99edc7a48ebeba4d48cf0a4ec927ae077c068
bbdnet2497 <zaeema@bbd.co.za>
2022-07-13 18:25:14 +0200

    updated deployment scripts

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,9 +5,15 @@ cd /home/ec2-user/app
 # end any running apps
 pm2 delete all
 
+pm2 start npm --name "tetris" -- start
+pm2 startup
+pm2 save
+pm2 restart all
+
 # start app
 # npm start
-pm2 start npm --name "tetris" -- start
+cd src/server
+pm2 start node api-server.js
 pm2 startup
 pm2 save
 pm2 restart all
